20e4ef1440242bf6fdde083fc6fe21029f29fff3
Erik Alfredsson <erik.alfredsson.jb@gmail.com>
2020-08-16 12:03:05 +0200

    Add cors for github pages

--- server.js
+++ server.js
@@ -1,12 +1,25 @@
 var express = require("express");
+var cors = require('cors');
 var app = express();
-var ufc = require('./services/ufc')
+var ufc = require('./services/ufc');
+
+var whitelist = ['https://alfredssonerik.github.io'];
+
+var corsOptions = {
+  origin: function (origin, callback) {
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
 
 app.listen(process.env.PORT || 3000, () => {
 	console.log("Server running on env port or port 3000");
 });
 
-app.get("/athlete/:name", (req, res, next) => {
+app.get("/athlete/:name", cors(corsOptions), (req, res, next) => {
 	console.log('Fighter: retrieving...');
 	ufc.getFighter(req.params.name)
 	.then(result => {
@@ -18,7 +31,7 @@ app.get("/athlete/:name", (req, res, next) => {
 	})
 });
 
-app.get("/rankings", (req, res, next) => {
+app.get("/rankings", cors(corsOptions), (req, res, next) => {
 	console.log('Rankings: retrieving...');
 	ufc.getRankings()
 	.then(result => {
